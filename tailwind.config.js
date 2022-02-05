9a6e1060ecd36a894827d3e1a6fb15a33cad66d3
Hojjat <h.galeh@gmail.com>
2022-02-05 11:03:56 +0300

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class' or false
+  theme: {
+    extend: {},
+  },
+  content: [
+    "./src/**/*.{html, ts}",
+  ],
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
