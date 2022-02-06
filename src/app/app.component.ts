59608dcc13657c141e5d71bb5d712aab2643df21
Hojjat <h.galeh@gmail.com>
2022-02-06 13:34:46 +0300

    add icon to sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public mobileQuery: MediaQueryList;
 
-  constructor(media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-  }
+  constructor() { }
 }
